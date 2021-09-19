cb7b24782b1e41489e502644ba842fb544845ce6
Pedro Henrique Vieira Fernandes <pedrohv20fernandes@gmail.com>
2021-09-19 02:48:58 -0300

    Att HTML e CSS 19/09

--- TerceiroSemestre/POO-Waldir/12- POO-Laboratório 7 Padrões Estruturais/ConversorStringtoAscii/src/String1.java
+++ TerceiroSemestre/POO-Waldir/12- POO-Laboratório 7 Padrões Estruturais/ConversorStringtoAscii/src/String1.java
@@ -4,18 +4,18 @@ public class String1 {
     int i;
 // o especificador de formato para caractere (%c)
 // o especificador de formato para decimal (%d)
-// mostra o i-ésimo caractere da tabela ASCII
-    System.out.printf("Caracteres numéricos:\n");
+// mostra o i-ï¿½simo caractere da tabela ASCII
+    System.out.printf("Caracteres numï¿½ricos:\n");
     for (i=48; i<=57; i++) {
       System.out.printf("%d", i);
     }
 
-    System.out.printf("\n\nCaracteres alfabéticos maiúsculos:\n");
+    System.out.printf("\n\nCaracteres alfabï¿½ticos maiï¿½sculos:\n");
     for (i=65; i<=90; i++) {
       System.out.printf("%d", i);
     }
 
-    System.out.printf("\n\nCaracteres alfabéticos minúsculos:\n");
+    System.out.printf("\n\nCaracteres alfabï¿½ticos minï¿½sculos:\n");
     for (i=97; i<=122; i++) {
       System.out.printf("%d", i);
     }
