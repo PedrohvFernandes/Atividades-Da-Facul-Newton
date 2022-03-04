c528ce1c1028adde6b8cd486e909342243236fc1
Pedro <pedrohv20fernandes@gmail.com>
2022-03-04 15:28:26 -0300

    Ajustes

--- QuintoSemestre/Arquitetura de aplicações Web/OutrasAtividades/ExemploAula03-03/server.js
+++ QuintoSemestre/Arquitetura de aplicações Web/OutrasAtividades/ExemploAula03-03/server.js
@@ -1,11 +1,14 @@
-// Usando MVC Model (modelo) View (visão HTML DINAMICO AJS) e Controller (Controle)
+// Usando MVC Model (modelo) View (visão HTML DINAMICO EJS) e Controller (Controle)
 
-// Tem tambem o -> O MVVM (Model-View-ViewModel) é um padrão de design de arquitetura de interface do usuário para desacoplamento da interface do usuário e de código que não é da interface do usuário.há 7 dias
+// Tem tambem o -> O MVVM (Model-View-ViewModel) é um padrão de design de arquitetura de interface do usuário para desacoplamento da interface do usuário e de código que não é da interface do usuário.
+
+// Rota => ela serve pra pegar get, fazer post... Pro controllers que recebe a requisição da rota e com essa rquisição ele direciona pro model que tem a função pra carregar o JSON pra carregar na View que esta o HTML, nesse caso é HTML dinamico
 
 // Requisitando o express. Obs: o express tem que estar insalado
 const EXPRESS = require("express");
 
-const CONSIGN = require('consign');
+// Ele da autoload nos meus scripts, ele é quem executa os meus caminhos -> /rota /views /...
+const CONSIGN = require("consign");
 
 // A minha aplicação vai conseguir herdar as caracteristicas do Express, conseguindo fazer requisição e resposta
 // Com isso o server vira um objeto do tipo express
@@ -27,6 +30,7 @@ const SERVER = EXPRESS();
 //   );
 // });
 
+// O get agora fica na rota
 // SERVER.get("/user", (req, res) => {
 //     return res.json([
 //       {
@@ -41,14 +45,15 @@ const SERVER = EXPRESS();
 //     );
 //   });
 
+// Configuração do que eu vou utilizar qual o html que vamos usar no view no caso o dinamico ejs
+SERVER.set("view engine", "ejs");
+SERVER.set("views", "./src/views");
 
-SERVER.set('view engine', 'ejs');
-SERVER.set('views', './src/views')
-
+// Pegando os caminhos pra executar os views de modo async, porque que vai da start agora é ele e não nodemon
 CONSIGN()
-  .include('src/routes')
-  .then('src/models')
-  .then('src/controllers')
+  .include("src/routes")
+  .then("src/models")
+  .then("src/controllers")
   .into(SERVER);
 
 //servidor rodando na porta, a gente vai usar o script start para iniciar o projeto, nele a gente define o nodemon para iniciar o projeto -> Npm start
--- QuintoSemestre/Arquitetura de aplicações Web/OutrasAtividades/ExemploAula03-03/src/controllers/login.js
+++ QuintoSemestre/Arquitetura de aplicações Web/OutrasAtividades/ExemploAula03-03/src/controllers/login.js
@@ -1,7 +1,7 @@
-module.exports.index = function(application, req, res) {
-    var loginModel = new application.src.models.login();
-  
-    loginModel.getLogin(function(err, result) {
-      res.render("login/index", {login : result});
-    });
-  }
+module.exports.index = function (application, req, res) {
+  var loginModel = new application.src.models.login();
+
+  loginModel.getLogin(function (err, result) {
+    res.render("login/index", { login: result });
+  });
+};
